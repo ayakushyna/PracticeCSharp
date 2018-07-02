4c3cbc9440b825d090fef4de29584e93b50d9d9d
Anastasia <ayakushina07@gmail.com>
2018-07-03 00:22:10 +0300

    Made task3.4

---
+++ Lab3/Task3.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shop> shops = new List<Shop>
+            {
+                new Shop(111, "Computer", "kg", 500, new DateTime(2015, 7, 20), 20, 15, 30),
+                new Shop(112, "Ball", "kg", 4.8, new DateTime(2015, 7, 20), 30, 10, 40),
+                new Shop(211, "Glasses", "kg", 20, new DateTime(2015, 7, 20), 40, 18, 50),
+                new Shop(121, "Phone", "kg", 125.5, new DateTime(2015, 7, 21), 50, 12, 60),
+                new Shop(311, "Pen", "kg", 3.7, new DateTime(2015, 7, 20), 60, 19, 70)
+            };
+
+            DateTime date = new DateTime(2015, 7, 20);
+            var specShops = shops.Where(x => (x.CurrentDate == date));
+
+            foreach(var shop in specShops)
+            {
+                Console.WriteLine("Product Name: {0},Balance Of Goods At Begin: {1},Received Goods: {2}, Sold Goods {3}",
+                    shop.ProductName, shop.BalanceOfGoodsAtBegin, shop.ReceivedGoods, shop.SoldGoods);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab3/Task3.4/Shop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._4
+{
+    class Shop
+    {
+        public int ProductCode { get; }
+        public string ProductName { get; }
+        public string Measurement { get; }
+        public double ProductPrice { get; }
+        public DateTime CurrentDate { get; }
+        public int BalanceOfGoodsAtBegin { get; }
+        public int ReceivedGoods { get; }
+        public int SoldGoods { get; }
+
+        public Shop(int productCode, string productName, string measurement, double productPrice, 
+            DateTime currentDate, int balanceOfGoodsAtBegin, int receivedGoods, int soldGoods)
+        {
+            ProductCode = productCode;
+            ProductName = productName;
+            Measurement = measurement;
+            ProductPrice = productPrice;
+            CurrentDate = currentDate;
+            BalanceOfGoodsAtBegin = balanceOfGoodsAtBegin;
+            ReceivedGoods = receivedGoods;
+            SoldGoods = soldGoods;
+        }
+    }
+}
