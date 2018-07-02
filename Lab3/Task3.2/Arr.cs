f2c5ebc6dfbe5fd08ffe36c0e49f01efbcd42cbf
Anastasia <ayakushina07@gmail.com>
2018-07-02 21:29:06 +0300

    Made task3.2

---
+++ Lab3/Task3.2/Arr.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._2
+{
+    class Arr
+    {
+        private int n;
+        private int[] array;
+        private int lessThan50;
+
+        public Arr(int n, int x1, int d)
+        {
+            this.n = n;
+            array = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = x1 + d * i;
+            }
+        }
+
+        public Arr(string str)
+        {
+            string[] numbers = str.Split(new char[] { ';' });
+            this.n = numbers.Length;
+            array = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(numbers[i]);
+            }
+
+        }
+
+        public int LessThan50
+        {
+            get
+            { 
+                lessThan50 = 0;
+
+                foreach(int number in array)
+                {
+                    if (number < 50) lessThan50++;
+                }
+
+                return lessThan50;
+            }
+        }
+
+        public void Print()
+        {
+            foreach (int number in array)
+            {
+                Console.Write("{0} | ", number);
+            }
+            Console.WriteLine();
+        }
+
+        public int AbsMultiplication()
+        {
+            int result = 1,i = 0;
+            bool t = false;
+
+            while (i < n)
+            {
+                if(array[i] % 2 == 0)
+                {
+                    t = true;
+                    break;
+                }
+
+                i++;
+            }
+
+            for ( ; i < n; i++)
+            {
+                result *= Math.Abs(array[i]);
+            }
+
+            return t? result: 0;
+        }
+    }
+}
---
+++ Lab3/Task3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arr arr1 = new Arr(5, 1, 21);
+            arr1.Print();
+
+            Console.WriteLine("Multiplication of the absolute values of the elements located after the first even element: {0}", arr1.AbsMultiplication());
+            Console.WriteLine("Number of elements that are less than 50: {0}", arr1.LessThan50);
+
+            string str = "123;1;456;23;2;-54;32;-9";
+            Arr arr2 = new Arr(str);
+            arr2.Print();
+
+            Console.WriteLine("Multiplication of the absolute values of the elements located after the first even element: {0}", arr2.AbsMultiplication());
+            Console.WriteLine("Number of elements that are less than 50: {0}", arr2.LessThan50);
+
+            Console.ReadKey();
+        }
+    }
+}
