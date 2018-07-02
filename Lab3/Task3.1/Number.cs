2ef2ac66927df0bafb3e455966bfed196f1559b6
Anastasia <ayakushina07@gmail.com>
2018-07-02 19:30:43 +0300

    Made task3.1

---
+++ Lab3/Task3.1/Number.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._1
+{
+    class Number
+    {
+        private int integer;
+
+        public Number(int integer)
+        {
+            this.integer = integer;
+        }
+
+        public bool Prime()
+        {
+            for(int i = 2; i <= Math.Sqrt(integer); i++)
+		        if (integer % i == 0)
+                    return false;
+            return true;
+        }
+
+        public bool Palindrome()
+        {
+            if( integer == Reverse(integer))
+                    return true;
+            return false;
+        }
+
+        private int Reverse(int num)
+        {
+            int rNum = 0;
+            while (num != 0)
+            {
+                rNum = rNum * 10 + num % 10;
+                num /= 10;
+            }
+            return rNum;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Integer = {0}", integer);
+        }
+    }
+}
---
+++ Lab3/Task3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int integer = random.Next();
+
+            Number number = new Number(integer);
+            number.Print();
+
+            Console.WriteLine("Prime number: {0}", number.Prime());
+            Console.WriteLine("Palindrome: {0}", number.Palindrome());
+
+            Console.ReadKey();
+        }
+    }
+}
