ba5fe0169286817b4cd04a49d33d7c122982f59a
Anastasia <ayakushina07@gmail.com>
2018-07-05 00:08:59 +0300

    Made task7.2

---
+++ Lab7/Task7.2/Task7.2/Form1.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace Task7._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Task7._2.Properties.Resources.wf1;
+            this.ClientSize = new System.Drawing.Size(398, 444);
+            this.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Lab7/Task7.2/Task7.2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task7._2
+{
+    public partial class Form1 : Form
+    {
+        private Button[,] buttons = new Button[4,3];
+        private TextBox textBox;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox = new TextBox
+            {
+                ReadOnly = true,
+                Location = new Point(60, 50),
+                Size = new Size(300, 20)
+            };
+            Controls.Add(textBox);
+
+            int number = 0;
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j] = new Button
+                    {
+                        Size = new Size(80, 50),
+                        Location = new Point(70 + j * 100, 100 + i * 70)
+                    };
+
+                    if (number < 10) buttons[i, j].Text = number.ToString();
+                    else if (number == 10)
+                    {
+                        buttons[i, j].Text = "Корень";
+                    }
+                    else buttons[i, j].Text = "C";
+
+                    number++;
+
+                    buttons[i, j].Click += new EventHandler(buttons_Click);
+                    Controls.Add(buttons[i, j]);
+                }
+        }
+
+        private void buttons_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            try
+            {
+                if (button.Text.Length == 1 && button.Text != "C")
+                {
+                    if (textBox.Text.Contains(","))
+                        textBox.Clear();
+                    textBox.Text += button.Text;
+                }
+                else if (button.Text == "C") textBox.Clear();
+                else textBox.Text = String.Format("{0:F5}", Math.Sqrt(Convert.ToDouble(textBox.Text)));
+            }
+            catch
+            {
+                MessageBox.Show("Введите число");
+            }
+        }
+        
+    }
+}
